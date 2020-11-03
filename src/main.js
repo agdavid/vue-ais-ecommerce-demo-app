241ca9622c08ed52932f32b0097900bb7071a141
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2020-11-03 09:00:37 -0500

    include Vue IS library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import InstantSearch from 'vue-instantsearch';
 
 Vue.config.productionTip = false
 
+Vue.use(InstantSearch);
+
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+});
